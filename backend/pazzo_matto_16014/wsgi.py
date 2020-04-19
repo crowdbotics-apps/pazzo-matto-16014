0e6eaf8cbed83c5f4e68ea21524f3c90c2efb7ea
Crowdbotics <team@crowdbotics.com>
2020-04-19 15:37:37 +0000

    Initial commit.

---
+++ backend/pazzo_matto_16014/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for pazzo_matto_16014 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pazzo_matto_16014.settings')
+
+application = get_wsgi_application()
